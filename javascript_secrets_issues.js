c95ba7e1cd1888b81a13aa7a2984a48435b4f88e
EbalYaVRotMicrosoft <mrrrkatykaty1998@yandex.ru>
2023-02-12 15:43:00 +0300

    updated js branch

---
+++ javascript_secrets_issues.js
@@ -0,0 +1,7 @@
+STEAM_API_KEY = "kdjfh_dew43sslllelwelwlew"
+
+username : "admin",
+password: "pass",
+
+
+xhttp.send(JSON.stringify({ 'email': 'testing@test.com', 'password': 'pwned', 'isAdmin': true }));
